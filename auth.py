0b4c5fe4719d54e39cd05abd0f2e3d356a3ee59b
YESSENIAJUAREZHERNANDEZ <1720110342@utectulancingo.edu.mx>
2022-04-10 17:15:18 +0000

    o

--- auth.py
+++ auth.py
@@ -1,9 +1,6 @@
 import web
 import pyrebase
 import firebase_config as token
-firebase = pyrebase.initialize_app(token.firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
 urls = (
     '/registrar', 'Registrar'
 )
@@ -16,24 +13,30 @@ class Registrar:
         return render.registrar()
 
     def POST(self):
-        formulario = web.input()
-        auth = firebase.auth()
-        db = firebase.database()
-        name = formulario.name
-        phone = formulario.phone
-        email = formulario.email
-        password = formulario.password
-        print(email,password)
-        user = auth.create_user_with_email_and_password(email, password)
-        print('localId: ',user['localId'])
-        data ={
-            "nombre": name,  
-            "phone": phone, 
-            "email": email
-        }
-        results = db.child("users").child(user['localId']).set(data)
-        print(results)
-
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig)
+            auth = firebase.auth()
+            db = firebase.database()
+            formulario = web.input()
+            auth = firebase.auth()
+            db = firebase.database()
+            name = formulario.name
+            phone = formulario.phone
+            email = formulario.email
+            password = formulario.password
+            print(email,password)
+            user = auth.create_user_with_email_and_password(email, password)
+            local_id =(user['localId'])
+            data ={
+                "nombre": name,  
+                "phone": phone, 
+                "email": email
+            }
+            results = db.child("users").child(user['localId']).set(data)
+            return web.seeother("/")
+            print(results)
+            except Exception as error:
+                
         return render.registrar()
     
 if __name__ == "__main__":
